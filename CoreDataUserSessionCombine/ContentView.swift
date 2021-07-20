e264fe06139fc47bc9b072afea4bc7e8d65d41b5
Pavel Uvarov <p.uvarov@gmail.com>
2021-07-20 11:35:21 -0400

    Initial Commit

---
+++ CoreDataUserSessionCombine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataUserSessionCombine
+//
+//  Created by Pavel Uvarov on 2021-07-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
